039d41cc3c445fe9a672e95a7b7bd4f0d9a32a8b
Alex Iankoulski <alex_iankoulski@yahoo.com>
2021-04-23 09:40:42 -0700

    initial commit

---
+++ exec.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source .env
+
+if [ "$1" == "" ]; then
+	CMD="sh -c 'if [ -e /bin/bash ]; then /bin/bash; else sh; fi'"
+else
+	CMD=$@
+fi
+
+CMD="docker container exec -it ${CONTAINER} $CMD"
+
+eval "$CMD"
+
